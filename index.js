926189ba0eb71333eed76b08e63db01a2263549c
Sarah <spfohman@email.essex.edu>
2021-09-09 16:17:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name){  
+    //var name="Bobby"
+    return drivers.filter(obj =>
+    obj.toLowerCase().indexOf(name.toLowerCase()) >= 0);
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(obj=>
+        obj.toLowerCase().indexOf(letters.toLowerCase())===0);
+}
+
+function matchName(drivers, name){
+    return drivers.filter(obj=> obj.name.includes(name));
+    
+
+}
